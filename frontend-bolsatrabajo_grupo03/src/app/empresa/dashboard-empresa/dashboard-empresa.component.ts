87fe28cd1d13095c11e441d9dd89d301a7bba77c
obed011 <158543664+obed011@users.noreply.github.com>
2025-06-16 09:18:24 -0600

    mostrar nombre empresa

--- frontend-bolsatrabajo_grupo03/src/app/empresa/dashboard-empresa/dashboard-empresa.component.ts
+++ frontend-bolsatrabajo_grupo03/src/app/empresa/dashboard-empresa/dashboard-empresa.component.ts
@@ -161,4 +161,13 @@ export class DashboardEmpresaComponent implements OnInit {
   isActive(itemId: string): boolean {
     return this.activeMenuItem === itemId;
   }
+  getCurrentDateTime(): string {
+    return new Date().toLocaleString('es-ES', {
+      day: '2-digit',
+      month: '2-digit', 
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
+    });
+  }
 }
