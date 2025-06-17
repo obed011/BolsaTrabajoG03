515c48c83e5e14610e32f00398e064f35e6492ef
obed011 <158543664+obed011@users.noreply.github.com>
2025-06-17 10:20:47 -0600

    correccion de estilos visuales

--- frontend-bolsatrabajo_grupo03/src/app/pages/inicio/inicio.component.ts
+++ frontend-bolsatrabajo_grupo03/src/app/pages/inicio/inicio.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   imports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RouterModule
   ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
